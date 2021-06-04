02c28b30de6d11025ae7d85d1a39614640dbef24
CyberTerrorist-x <84683727+CyberTerrorist-x@users.noreply.github.com>
2021-06-04 19:05:39 -0300

    cep.py

---
+++ cep.py
@@ -0,0 +1,34 @@
+import requests
+
+zz = open('newfile.txt','r')
+ll = zz.read()
+print(ll)
+print(' ')
+
+print("")
+print("Informe o CEP sem traços! ")
+print(' ')
+
+while True:
+	x = input('CEP: ')
+	if len(x) !=8:
+		print(f"Erro in {x}")
+	request = requests.get(f'https://viacep.com.br/ws/{x}/json/')
+		
+	address_data = request.json()
+	if 'erro' not in address_data:
+		print(' ')
+		print('CEP: {}'.format(address_data['cep']))
+		print('Logradouro: {}'.format(address_data['logradouro']))
+		print('Complemento: {}'.format(address_data['complemento']))
+		print('Bairro: {}'.format(address_data['bairro']))
+		print('Localidade: {}'.format(address_data['localidade']))
+		print('IBGE: {}'.format(address_data['ibge']))
+		print('UF: {}'.format(address_data['uf']))
+		print('GIA: {}'.format(address_data['gia']))
+		print('DDD: {}'.format(address_data['ddd']))
+		print('Siafi: {}'.format(address_data['siafi']))
+		print(' ')
+		
+	else:
+		print('erro')
